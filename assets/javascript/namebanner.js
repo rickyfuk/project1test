87dd82df6e7aa21b33d34b48e824644a5833aefe
digilau <rickyfukfuk@gmail.com>
2020-08-11 19:56:42 -0700

    add extra content

--- assets/javascript/namebanner.js
+++ assets/javascript/namebanner.js
@@ -1,300 +1,30 @@
-// var count = 0;
-// var genderCount = 0;
-// var raceCount = 0;
-// var classCount = 0;
-
-// Arrays with images
-// var mhf = [
-// 	'images-male/human-fighter-1.jpg',
-// 	'images-male/human-fighter-2.jpg',
-// 	'images-male/human-fighter-3.jpg',
-// ];
-// var mhra = [
-// 	'images-male/human-ranger-1.jpg',
-// 	'images-male/human-ranger-2.jpg',
-// 	'images-male/human-ranger-3.jpg',
-// ];
-// var mhm = [
-// 	'images-male/human-mage-1.jpg',
-// 	'images-male/human-mage-2.jpg',
-// 	'images-male/human-mage-3.jpg',
-// ];
-// var mhc = [
-// 	'images-male/human-cleric-1.jpg',
-// 	'images-male/human-cleric-2.jpg',
-// 	'images-male/human-cleric-3.jpg',
-// ];
-// var mhro = [
-// 	'images-male/human-rogue-1.jpg',
-// 	'images-male/human-rogue-2.jpg',
-// 	'images-male/human-rogue-3.jpg',
-// ];
-// var fhf = [
-// 	'images-female/human-fighter-1.jpg',
-// 	'images-female/human-fighter-2.jpg',
-// 	'images-female/human-fighter-3.jpg',
-// ];
-// var fhra = [
-// 	'images-female/human-ranger-1.jpg',
-// 	'images-female/human-ranger-2.jpg',
-// 	'images-female/human-ranger-3.jpg',
-// ];
-// var fhm = [
-// 	'images-female/human-mage-1.jpg',
-// 	'images-female/human-mage-2.jpg',
-// 	'images-female/human-mage-3.jpg',
-// ];
-// var fhc = [
-// 	'images-female/human-cleric-1.jpg',
-// 	'images-female/human-cleric-2.jpg',
-// 	'images-female/human-cleric-3.jpg',
-// ];
-// var fhro = [
-// 	'images-female/human-rogue-1.jpg',
-// 	'images-female/human-rogue-2.jpg',
-// 	'images-female/human-rogue-3.jpg',
-// ];
-// var mef = [
-// 	'images-male/elf-fighter-1.jpg',
-// 	'images-male/elf-fighter-2.jpg',
-// 	'images-male/elf-fighter-3.jpg',
-// ];
-// var fef = [
-// 	'images-female/elf-fighter-1.jpg',
-// 	'images-female/elf-fighter-2.jpg',
-// 	'images-female/elf-fighter-3.jpg',
-// ];
-// var mera = [
-// 	'images-male/elf-ranger-1.jpg',
-// 	'images-male/elf-ranger-2.jpg',
-// 	'images-male/elf-ranger-3.jpg',
-// ];
-// var fera = [
-// 	'images-female/elf-ranger-1.jpg',
-// 	'images-female/elf-ranger-2.jpg',
-// 	'images-female/elf-ranger-3.jpg',
-// ];
-// var mem = [
-// 	'images-male/elf-mage-1.jpg',
-// 	'images-male/elf-mage-2.jpg',
-// 	'images-male/elf-mage-3.jpg',
-// ];
-// var fem = [
-// 	'images-female/elf-mage-1.jpg',
-// 	'images-female/elf-mage-2.jpg',
-// 	'images-female/elf-mage-3.jpg',
-// ];
-// var mec = [
-// 	'images-male/elf-cleric-1.jpg',
-// 	'images-male/elf-cleric-2.jpg',
-// 	'images-male/elf-cleric-3.jpg',
-// ];
-// var fec = [
-// 	'images-female/elf-cleric-1.jpg',
-// 	'images-female/elf-cleric-2.jpg',
-// 	'images-female/elf-cleric-3.jpg',
-// ];
-// var mero = [
-// 	'images-male/elf-rogue-1.jpg',
-// 	'images-male/elf-rogue-2.jpg',
-// 	'images-male/elf-rogue-3.jpg',
-// ];
-// var fero = [
-// 	'images-female/elf-rogue-1.jpg',
-// 	'images-female/elf-rogue-2.jpg',
-// 	'images-female/elf-rogue-3.jpg',
-// ];
-// var mdf = [
-// 	'images-male/dwarf-fighter-1.jpg',
-// 	'images-male/dwarf-fighter-2.jpg',
-// 	'images-male/dwarf-fighter-3.jpg',
-// ];
-// var fdf = [
-// 	'images-female/dwarf-fighter-1.jpg',
-// 	'images-female/dwarf-fighter-2.jpg',
-// 	'images-female/dwarf-fighter-3.jpg',
-// ];
-// var mdra = [
-// 	'images-male/dwarf-ranger-1.jpg',
-// 	'images-male/dwarf-ranger-2.jpg',
-// 	'images-male/dwarf-ranger-3.jpg',
-// ];
-// var fdra = [
-// 	'images-female/dwarf-ranger-1.jpg',
-// 	'images-female/dwarf-ranger-2.jpg',
-// 	'images-female/dwarf-ranger-3.jpg',
-// ];
-// var mdm = [
-// 	'images-male/dwarf-mage-1.jpg',
-// 	'images-male/dwarf-mage-2.jpg',
-// 	'images-male/dwarf-mage-3.jpg',
-// ];
-// var fdm = [
-// 	'images-female/dwarf-mage-1.jpg',
-// 	'images-female/dwarf-mage-2.jpg',
-// 	'images-female/dwarf-mage-3.jpg',
-// ];
-// var mdc = [
-// 	'images-male/dwarf-cleric-1.jpg',
-// 	'images-male/dwarf-cleric-2.jpg',
-// 	'images-male/dwarf-cleric-3.jpg',
-// ];
-// var fdc = [
-// 	'images-female/dwarf-cleric-1.jpg',
-// 	'images-female/dwarf-cleric-2.jpg',
-// 	'images-female/dwarf-cleric-3.jpg',
-// ];
-// var mdro = [
-// 	'images-male/dwarf-rogue-1.jpg',
-// 	'images-male/dwarf-rogue-2.jpg',
-// 	'images-male/dwarf-rogue-3.jpg',
-// ];
-// var fdro = [
-// 	'images-female/dwarf-rogue-1.jpg',
-// 	'images-female/dwarf-rogue-2.jpg',
-// 	'images-female/dwarf-rogue-3.jpg',
-// ];
-
-// sets the image equal to the ID in the html
-// var result = document.getElementById('centerVidImg');
-
-// searches a random number and is called in each if then statement
-// function randomNumber(min, max) {
-// 	return Math.floor(Math.random() * (max - min) + min);
-// }
-
-// generates a random character image based on gender, race, class.
-// function randomImage() {
-
-// 	// plugs in to the local storage
-// 	var gender = localStorage.getItem("charGender");
-// 	var race = localStorage.getItem("charRace");
-// 	var classDisplay = localStorage.getItem("charClass");
-// 	// display which plugs into var results
-// 	var currentpicture;
-
-// 	// human if then statements "Male"
-// 	if (gender === "Male" && race === "Human" && classDisplay === "Fighter") {
-// 		currentpicture = mhf[randomNumber(0,mhf.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Human" && classDisplay === "Ranger") {
-// 		currentpicture = mhra[randomNumber(0,mhra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Human" && classDisplay === "Wizard") {
-// 		currentpicture = mhm[randomNumber(0,mhra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Human" && classDisplay === "Cleric") {
-// 		currentpicture = mhc[randomNumber(0,mhra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Human" && classDisplay === "Rogue") {
-// 		currentpicture = mhro[randomNumber(0,mhra.length)];
-// 		result.src = currentpicture;
-
-// 	// human if then statements "Female"
-// 	} else if (gender === "Female" && race === "Human" && classDisplay === "Fighter") {
-// 		currentpicture = fhf[randomNumber(0,fhf.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Human" && classDisplay === "Ranger") {
-// 		currentpicture = fhra[randomNumber(0,fhra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Human" && classDisplay === "Wizard") {
-// 		currentpicture = fhm[randomNumber(0,fhm.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Human" && classDisplay === "Cleric") {
-// 		currentpicture = fhc[randomNumber(0,fhc.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Human" && classDisplay === "Rogue") {
-// 		currentpicture = fhro[randomNumber(0,fhro.length)];
-// 		result.src = currentpicture;
-
-// 	// elf if then statements "Male"
-// 	} else if (gender === "Male" && race === "Elf" && classDisplay === "Fighter") {
-// 		currentpicture = mef[randomNumber(0,mef.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Elf" && classDisplay === "Ranger") {
-// 		currentpicture = mera[randomNumber(0,mera.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Elf" && classDisplay === "Wizard") {
-// 		currentpicture = mem[randomNumber(0,mera.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Elf" && classDisplay === "Cleric") {
-// 		currentpicture = mec[randomNumber(0,mera.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Elf" && classDisplay === "Rogue") {
-// 		currentpicture = mero[randomNumber(0,mera.length)];
-// 		result.src = currentpicture;
-
-// 	// elf if then statements "Female"
-// 	} else if (gender === "Female" && race === "Elf" && classDisplay === "Fighter") {
-// 		currentpicture = fef[randomNumber(0,fef.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Elf" && classDisplay === "Ranger") {
-// 		currentpicture = fera[randomNumber(0,fera.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Elf" && classDisplay === "Wizard") {
-// 		currentpicture = fem[randomNumber(0,fem.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Elf" && classDisplay === "Cleric") {
-// 		currentpicture = fec[randomNumber(0,fec.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Elf" && classDisplay === "Rogue") {
-// 		currentpicture = fero[randomNumber(0,fero.length)];
-// 		result.src = currentpicture;
-
-// 	// dwarf if then statements "Male"
-// 	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Fighter") {
-// 		currentpicture = mdf[randomNumber(0,mdf.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Ranger") {
-// 		currentpicture = mdra[randomNumber(0,mdra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Wizard") {
-// 		currentpicture = mdm[randomNumber(0,mdra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Cleric") {
-// 		currentpicture = mdc[randomNumber(0,mdra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Male" && race === "Dwarf" && classDisplay === "Rogue") {
-// 		currentpicture = mdro[randomNumber(0,mdra.length)];
-// 		result.src = currentpicture;
-
-// 	// dwarf if then statements "Female"
-// 	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Fighter") {
-// 		currentpicture = fdf[randomNumber(0,fdf.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Ranger") {
-// 		currentpicture = fdra[randomNumber(0,fdra.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Wizard") {
-// 		currentpicture = fdm[randomNumber(0,fdm.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Cleric") {
-// 		currentpicture = fdc[randomNumber(0,fdc.length)];
-// 		result.src = currentpicture;
-// 	} else if (gender === "Female" && race === "Dwarf" && classDisplay === "Rogue") {
-// 		currentpicture = fdro[randomNumber(0,fdro.length)];
-// 		result.src = currentpicture;
-// 	}
-
-// 	weaponsSpells();
-
-// 	//updates the src.URL to equal the image
-// 	$("vidImageDiv").attr("src",result.src);
-// };
-
+// Function to load character information from Local Storage if any exists
 $(window).on('load', function () {
+
+	//If statement to check if Local Storage has charcater info
 	if (localStorage.charClass != null) {
+		
+		//Get info from local storage
 		var name = localStorage.getItem('charName');
 		var gender = localStorage.getItem('charGender');
 		var race = localStorage.getItem('charRace');
 		var classDisplay = localStorage.getItem('charClass');
 
+		//Display info to html
 		$('#characterName').text(name);
 		$('#gender').text(gender);
 		$('#race').text(race);
 		$('#class').text(classDisplay);
+
+		//Calls functions to generate image, stats and weapons/skills
+		weaponsSpells();
 		randomImage();
-	} else {
+		stats();
+	}
+	
+	else {
+
+		//Generates name and creates dropdown for Gender, Race and Class
 		generateName();
 		$('#gender').text('Gender');
 		$('#race').text('Race');
@@ -303,7 +33,11 @@ $(window).on('load', function () {
 	}
 });
 
+
+// Random Name Function
 function generateName() {
+
+	// Random Name API, ajax GET and Then function
 	var queryURL =
 		'https://cors-anywhere.herokuapp.com/https://uzby.com/api.php?min=3&max=10';
 
@@ -317,10 +51,11 @@ function generateName() {
 	});
 }
 
-// Allows Character Name to be edited after double click
+//Function to edit Character Name
 function editName() {
 	$('#characterName').attr('contenteditable', 'true');
 
+	// If 1st Double Click, save button will be generated
 	if (count === 0) {
 		var saveButton = $('<button>');
 		$(saveButton).text('Save');
@@ -331,6 +66,7 @@ function editName() {
 		count = count + 1;
 	}
 
+	// Save Buttton sets user typed name to name banner, Local Storage and resets count
 	$('#saveBtn').click(function () {
 		$('#characterName').attr('contenteditable', 'false');
 		$('#saveBtn').remove();
@@ -340,19 +76,20 @@ function editName() {
 	});
 }
 
+// Allows Character Name to be edited after double click
 $('#characterName').dblclick(function () {
 	editName();
 });
 
-// Allows user to edit Gender, Race and Class after double click
+// Allows user to edit Gender, Race and Class
 function editGenderRaceClass() {
+
+	//Dynamically creates dropdown
 	$('#addDrop').attr('data-dropdown-menu', '');
 	$('#removeGender').addClass('is-dropdown-submenu-parent opens-right');
 	$('#removeRace').addClass('is-dropdown-submenu-parent opens-right');
 	$('#removeClass').addClass('is-dropdown-submenu-parent opens-right');
-	$('#genderUL').addClass(
-		'menu submenu is-dropdown-submenu first-sub vertical'
-	);
+	$('#genderUL').addClass('menu submenu is-dropdown-submenu first-sub vertical');
 	$('#raceUL').addClass('menu submenu is-dropdown-submenu first-sub vertical');
 	$('#classUL').addClass('menu submenu is-dropdown-submenu first-sub vertical');
 
@@ -443,8 +180,10 @@ function editGenderRaceClass() {
 		$('#removeGender').removeClass();
 		$('#genderUL').removeClass();
 		randomImage();
+		stats();
 		var race = localStorage.getItem('charRace');
 		var classDisplay = localStorage.getItem('charClass');
+
 		if (race !== null) {
 			$('#dwarfList').remove();
 			$('#elfList').remove();
@@ -482,8 +221,10 @@ function editGenderRaceClass() {
 		$('#removeGender').removeClass();
 		$('#genderUL').removeClass();
 		randomImage();
+		stats();
 		var race = localStorage.getItem('charRace');
 		var classDisplay = localStorage.getItem('charClass');
+
 		if (race !== null) {
 			$('#dwarfList').remove();
 			$('#elfList').remove();
@@ -522,8 +263,10 @@ function editGenderRaceClass() {
 		$('#removeRace').removeClass();
 		$('#raceUL').removeClass();
 		randomImage();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var classDisplay = localStorage.getItem('charClass');
+
 		if (gender !== null) {
 			$('#femList').remove();
 			$('#maleList').remove();
@@ -561,8 +304,10 @@ function editGenderRaceClass() {
 		$('#removeRace').removeClass();
 		$('#raceUL').removeClass();
 		randomImage();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var classDisplay = localStorage.getItem('charClass');
+
 		if (gender !== null) {
 			$('#femList').remove();
 			$('#maleList').remove();
@@ -600,8 +345,10 @@ function editGenderRaceClass() {
 		$('#removeRace').removeClass();
 		$('#raceUL').removeClass();
 		randomImage();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var classDisplay = localStorage.getItem('charClass');
+
 		if (gender !== null) {
 			$('#femList').remove();
 			$('#maleList').remove();
@@ -642,8 +389,10 @@ function editGenderRaceClass() {
 		$('#classUL').removeClass();
 		randomImage();
 		weaponsSpells();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var race = localStorage.getItem('charRace');
+
 		if (gender !== null) {
 			$('#femList').remove();
 			$('#maleList').remove();
@@ -682,8 +431,10 @@ function editGenderRaceClass() {
 		$('#classUL').removeClass();
 		randomImage();
 		weaponsSpells();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var race = localStorage.getItem('charRace');
+
 		if (gender !== null) {
 			$('#femList').remove();
 			$('#maleList').remove();
@@ -722,6 +473,7 @@ function editGenderRaceClass() {
 		$('#classUL').removeClass();
 		randomImage();
 		weaponsSpells();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var race = localStorage.getItem('charRace');
 		if (gender !== null) {
@@ -762,6 +514,7 @@ function editGenderRaceClass() {
 		$('#classUL').removeClass();
 		randomImage();
 		weaponsSpells();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var race = localStorage.getItem('charRace');
 		if (gender !== null) {
@@ -802,6 +555,7 @@ function editGenderRaceClass() {
 		$('#classUL').removeClass();
 		randomImage();
 		weaponsSpells();
+		stats();
 		var gender = localStorage.getItem('charGender');
 		var race = localStorage.getItem('charRace');
 		if (gender !== null) {
@@ -826,14 +580,17 @@ function editGenderRaceClass() {
 	});
 }
 
+// Allows user to edit Gender, Race and Class after double click
 $('#gender').dblclick(function () {
 	editGenderRaceClass();
 });
 
+// Allows user to edit Gender, Race and Class after double click
 $('#race').dblclick(function () {
 	editGenderRaceClass();
 });
 
+// Allows user to edit Gender, Race and Class after double click
 $('#class').dblclick(function () {
 	editGenderRaceClass();
 });
